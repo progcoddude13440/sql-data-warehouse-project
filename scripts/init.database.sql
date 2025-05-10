525394d55bc74cbf408bc7d92658f1f2232783b1
Data Analyst James Hicks <jfh13440@hotmail.com>
2025-05-10 02:11:26 -0400

    Update init.database.sql

--- scripts/init.database.sql
+++ scripts/init.database.sql
@@ -12,12 +12,14 @@ with caution and ensure you have proper backyps before running this script.
 IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
 BEGIN ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
 END;
+GO
 */
 -- Create the 'DataWarehouse' database
 CREATE DATABASE DataWarehouse;
-
+GO
 USE DataWarehouse;
-
+GO
+-- Create Schemas
 CREATE SCHEMA bronze;
 GO
 CREATE SCHEMA silver;
